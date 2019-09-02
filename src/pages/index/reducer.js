720c229e22ac4b05197706c98325f7649d948c6a
Yuhang <yu6hang@gmail.com>
2019-09-02 10:41:28 +0800

    index rewrite

---
+++ src/pages/index/reducer.js
@@ -0,0 +1,53 @@
+export default function (state, action) {
+  let data;
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'FETCH_SUCCESS':
+      data = action.payload;
+      data = state.data
+        // 合并并去除重复项
+        ? state.data.concat(data.filter(
+          item => !(state.data.find(x => x.postId === item.postId) || false),
+        ))
+        : data;
+      data = data.sort((a, b) => b.postTime - a.postTime);
+      sessionStorage.setItem('postList', JSON.stringify(data.slice(0, 20)));
+
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        latestPostTime: data[0].postTime,
+        data,
+      };
+    case 'FETCH_INCROFFSET':
+      return {
+        ...state,
+        offsetCount: state.offsetCount + 1,
+      };
+    case 'FETCH_NONEWDATA':
+      return {
+        ...state,
+        isLoading: false,
+      };
+    case 'FETCH_NODATA':
+      return {
+        ...state,
+        hasMore: false,
+        isLoading: false,
+      };
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      throw new Error();
+  }
+};
---
+++ src/pages/index/timelineLatest.js
@@ -0,0 +1,142 @@
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useContext,
+} from 'react';
+// import LinearProgress from '@material-ui/core/LinearProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import InfiniteScroll from 'react-infinite-scroller';
+import instance from '../../components/axios';
+import Card from '../../components/Card';
+import userContext from '../../context/userContext';
+import dataFetchReducer from './reducer';
+import RefreshFab from './RefreshFab';
+
+const offsetBase = 20;
+
+const useDataApi = (initialUrl, initialData) => {
+  const context = useContext(userContext);
+  const [url, setUrl] = useState(initialUrl);
+
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    offsetCount: 0,
+    latestPostTime: 0,
+    hasMore: true,
+    data: initialData,
+  });
+
+  useEffect(() => {
+    let unmounted = false; // 防止组件卸载后，仍处理网络请求
+
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
+      try {
+        const result = await instance.get(url);
+
+        if (!unmounted) {
+          if (url.search('offset') !== -1) {
+            dispatch({ type: 'FETCH_INCROFFSET' });
+          }
+          if (result.data.length === 0) {
+            if (url.search('offset') !== -1) {
+              dispatch({ type: 'FETCH_NODATA' });
+            } else {
+              dispatch({ type: 'FETCH_NONEWDATA' });
+            }
+            context.setShowMsgBar('default', '没有更多帖子了');
+            return;
+          }
+          dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+        }
+      } catch (err) {
+        if (!unmounted) {
+          switch (err.response.status) {
+            case 401:
+              context.setShowMsgBar('fail', err.response.data.msg);
+              break;
+            case 403:
+              context.setShowMsgBar('fail', err.response.data.msg);
+              break;
+            case 500:
+              context.setShowMsgBar('fail', '服务器发生错误，请稍后再试。');
+              break;
+            case 502:
+              context.setShowMsgBar('fail', '服务器无响应，请稍后再试。');
+              break;
+            default:
+              context.setShowMsgBar('fail', `发生错误${err.response.status}`);
+              break;
+          }
+          dispatch({ type: 'FETCH_FAILURE' });
+        }
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      unmounted = true;
+    };
+  }, [url]);
+
+  return [state, setUrl];
+};
+
+export default function TimelineLatest() {
+  // offsetCount = 0;
+  let initData = sessionStorage.getItem('postList');
+  if (initData) {
+    initData = JSON.parse(initData);
+  } else {
+    initData = [];
+  }
+
+  const [{
+    data,
+    // isLoading,
+    // isError,
+    latestPostTime,
+    offsetCount,
+    hasMore,
+  }, doFetch] = useDataApi(
+    'getPost?offset=0',
+    initData,
+  );
+
+  function latestPostUrl() {
+    return `getPost?time=${latestPostTime}?rand=${Math.random()}`;
+  }
+
+  function nextPageUrl() {
+    return `getPost?offset=${offsetCount * offsetBase}`;
+  }
+
+  return (
+    <div>
+      <RefreshFab fetchNewData={() => doFetch(latestPostUrl)} />
+
+      {/* {isError && <div>发生错误</div>} */}
+      {/* {isLoading && <LinearProgress />} */}
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={() => doFetch(nextPageUrl)}
+        hasMore={hasMore}
+        threshold={400}
+        loader={<div style={{ textAlign: 'center', marginTop: 10 }} key={0}><CircularProgress /></div>}
+      >
+        {data && (
+          data.map(post => (
+            <Card
+              key={post.postId}
+              data={post}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </div>
+  );
+}
