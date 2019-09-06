86aa4364026f6f9cd1058a7d10ddf2c30fc30d38
Yuhang <yu6hang@gmail.com>
2019-09-06 11:37:34 +0800

    index tab init

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,8 +1,81 @@
+import AppBar from '@material-ui/core/AppBar';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Typography from '@material-ui/core/Typography';
 import React from 'react';
 import TimelineLatest from './timelineLatest';
+import TimelineHot from './timelineHot';
 
-export default function IndexPage() {
+function TabPanel(props) {
+  const {
+    children, value, index, ...other
+  } = props;
+
   return (
-    <TimelineLatest />
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box>{children}</Box>
+    </Typography>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  function handleChange(event, newValue) {
+    setValue(newValue);
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar
+        position="static"
+        color="default"
+      >
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+        >
+          <Tab label="最新发帖" {...a11yProps(0)} />
+          <Tab label="近期热门" {...a11yProps(1)} />
+          <Tab disabled label="更多" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <TimelineLatest />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <TimelineHot />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        ......
+      </TabPanel>
+    </div>
   );
 }
